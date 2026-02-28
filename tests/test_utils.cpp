7402400b745a674eabf84bb57873da589a85ee20
houcai <lishannng@hotmail.com>
2026-02-28 14:20:56 +0100

    test(utils): add tests for listToString

--- tests/test_utils.cpp
+++ tests/test_utils.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 
 using namespace utils;
+using namespace std::string_view_literals;
 
 TEST_CASE("toLower special-case ID", "[repository][utils]") {
     REQUIRE(lowercaseUntilCamelBoundary("ID") == "id");
@@ -31,4 +32,12 @@ TEST_CASE("toLower throws on upper-case string", "[repository][utils]") {
 
 TEST_CASE("toLower throws on trailing uppercase with no i+1", "[repository][utils]") {
     REQUIRE(lowercaseUntilCamelBoundary("functioN") == "function");
+}
+
+TEST_CASE("listToString: list of multiple elements", "[repository][utils]") {
+    REQUIRE(listToString(std::vector{"A"sv,"B"sv,"C"sv}) == "['A', 'B', 'C']");
+}
+
+TEST_CASE("listToString: list of single element", "[repository][utils]") {
+    REQUIRE(listToString(std::vector{"A"sv}) == "['A']");
 }
